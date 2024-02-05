192a42d1a6caac5e378d8d5604d87805c514fdff
Austinator65 <atisomofokeng167@gmail.com>
2024-02-05 16:04:54 +0200

    Added some scroll bar modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,15 +56,49 @@ const sendEmail = (e)=>{
       contactMessage.textContent = ''
 
     }, 5000)
+    contactForm.reset();
+
   }, ()=>{
     contactMessage.textContent = 'Message not sent (service error) ❌';
     setTimeout(()=>{
       contactMessage.textContent = ''
 
     }, 5000)
+
+    contactForm.reset();
   })
 }
 
 contactForm.addEventListener('submit', sendEmail)
 
 
+/*============================Show scroll up===================================== */
+
+const scrollUp = ()=>{
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >=350 ? scrollUp.classList.add('show-scroll'):scrollUp.classList.remove('show-scroll')
+
+}
+
+window.addEventListener('scroll', scrollUp);
+
+/*============================Scroll section active link===================================== */
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+window.addEventListener('scroll', scrollActive);
